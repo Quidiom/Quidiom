c86e648f39f525b282273cf60ee103ce68f5fb86
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2023-08-01 10:44:40 -0400

    create file structure for statController and authController and add boilerplate code

--- server/server.ts
+++ server/server.ts
@@ -2,14 +2,16 @@ import express, { Express, Request, Response } from 'express';
 const app: Express = express();
 const path = require('path');
 const port = 3000;
+const authController = require('./controllers/authController');
+const statController = require('./controllers/statController');
+
+// need to write route to render the front end from the build file (webpack output)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello From the Backend!');
-});
-
-
-
+  res.send('Hello From the Backend!')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  console.log(`Server is running at http://localhost:${port}`)
+})
+
