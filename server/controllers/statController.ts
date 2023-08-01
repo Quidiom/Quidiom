7c97712e2428fb5a77d64357ca1c9b18112bafb4
mitchgruen <103967990+mitchgruen@users.noreply.github.com>
2023-08-01 11:10:49 -0400

    add unkown route handler and global error handler

--- server/controllers/statController.ts
+++ server/controllers/statController.ts
@@ -7,7 +7,10 @@ statController.enterScore = (req: Request, res: Response, next: NextFunction) =>
 };
 
 statController.fetchLeaderboard = (req: Request, res: Response, next: NextFunction) => {
-  // 
+  // try to get * from SQL database
+  // format will be as an array of objects, where each object represents a row in the data set
+  // return the array of objects with next()
+  // if unable to fetch from db, invoke the global error handler
   return next()
 };
 
--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response,  } from 'express';
 const app: Express = express();
 const path = require('path');
 const port = 3000;
@@ -10,6 +10,24 @@ const statController = require('./controllers/statController');
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello From the Backend!')
 })
+
+// unknown route handler
+app.use((req, res) => {
+  res.status(404).send('Page not found')
+})
+
+// global error handler
+app.use((err: Error, req: Request, res: Response) => {
+  const defaultError = {
+    log: 'Middleware error',
+    status: 500,
+    message: 'Express caught an unknown error in middleware',
+  }
+  const errorObject = Object.assign(defaultError, err)
+  console.log(errorObject.log)
+  res.status(errorObject.status).json(errorObject.message)
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
